d1957e656854abad4f9dfbd24d0f3967af8c3c6c
khivi <khivi@users.noreply.github.com>
2021-09-16 12:27:23 -0400

    Added tailwindcss

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,20 @@
 import * as React from "react"
+import { Helmet } from 'react-helmet';
+
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+const Header = () => {
+ return (
+    <>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>cambrige.com</title>
+        <link rel='canonical' href='https://cambrige.com/' />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
+      </Helmet>
+    </>
+  );
 }
 
 export default Header
