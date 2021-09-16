5f05af47beb2d195c8b4f013cac628ab24cb4550
khivi <khivi@users.noreply.github.com>
2021-09-16 14:57:52 -0400

    use gatsby-plugin-image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,31 @@
 import * as React from "react"
 import { graphql, PageProps, Link } from "gatsby"
-import Img from 'gatsby-image';
-import { StaticImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Menu from "../components/menu"
 
-const IndexPage  = ({ data }) => (
-  <Layout>
+const IndexPage  = ({ data }) => {
+    const image = getImage(data.astronaut);
+  return (<Layout>
     <article className='prose'>
         <Menu/>
         <h1>Hi people</h1>
         <p>Welcome to your new Gatsby site.</p>
         <p>Now go build something great.</p>
-        <Img className={`float-right mx-8 z-0`} fixed={data.astronaut.childImageSharp.fixed} />
+        <GatsbyImage image={image} />
     </article>
-  </Layout>
-)
+  </Layout>);
+};
 
 export const query = graphql`
   fragment gatsbyImage on File {
     childImageSharp {
-        fixed(width: 200, height: 200) {
-              ...GatsbyImageSharpFixed
-        }
+        gatsbyImageData(
+            width: 200, 
+            height: 200,
+            layout: FIXED
+        )
       }
   }
   query {
